9a30d292d4b0fa2999579c696c987666254f9e4b
Tihons <tihonshramenko@gmail.com>
2024-07-02 12:18:52 +0200

    save

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import Title from "../components/Title/Title";
+
+export default function HomePage() {
+  return (
+    <>
+      <Title>PHONEBOOK</Title>
+
+      <div>
+        <h1>
+          Welcome to our app{" "}
+          <span role="img" aria-label="Greeting icon">
+            💁
+          </span>
+        </h1>
+      </div>
+    </>
+  );
+}
